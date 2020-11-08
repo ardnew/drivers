70f330ad0b8147479cec103ac1d817321f8df22a
ardnew <andrew@ardnew.com>
2020-11-08 13:50:03 -0600

    move I2C initialization to main

--- examples/ina260/main.go
+++ examples/ina260/main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"machine"
+	"time"
 
 	"tinygo.org/x/drivers/ina260"
 )
 
 func init() {
-	machine.I2C1.Configure(machine.I2CConfig{
-		Frequency: machine.TWI_FREQ_400KHZ,
-		SDA:       machine.I2C1_SDA_PIN,
-		SCL:       machine.I2C1_SCL_PIN,
-	})
 }
 
 // BUG
@@ -25,19 +21,27 @@ func init() {
 //     %3 = getelementptr inbounds { %machine.I2C }, { %machine.I2C }* %2, i32 0, i32 0, !dbg !4298
 //
 // (see other declaration below in `main`)
-var pow = ina260.New(machine.I2C1)
+//var pow = ina260.New(machine.I2C1)
 
 func main() {
+	machine.I2C1.Configure(machine.I2CConfig{
+		Frequency: machine.TWI_FREQ_400KHZ,
+		SDA:       machine.I2C1_SDA_PIN,
+		SCL:       machine.I2C1_SCL_PIN,
+	})
 
 	// BUG
 	// declaring the device here compiles OK
-	//var pow = ina260.New(machine.I2C1)
+	var pow = ina260.New(machine.I2C1)
 
 	println("initializing INA260 on I2C1")
 
-	if pow.Connected() {
-		println("connected: INA260")
-	} else {
-		println("INA260 not connected!")
+	for {
+		if pow.Connected() {
+			println("connected: INA260")
+		} else {
+			println("INA260 not connected!")
+		}
+		time.Sleep(2 * time.Second)
 	}
 }
